1390f486a8e238b9f2b9d40ea1978fa897e44fa5
chenchao <22190329366@qq.com>
2020-04-22 09:57:10 +0800

    更新

--- web-main/src/main/java/com/bateng/guestroom/GuestroomApplication.java
+++ web-main/src/main/java/com/bateng/guestroom/GuestroomApplication.java
@@ -18,6 +18,7 @@ public class GuestroomApplication {
     public static void main(String[] args) {
 
         System.out.println();
+        System.out.println();
         SpringApplication.run(GuestroomApplication.class, args);
 
     }
