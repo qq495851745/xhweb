3d1eee196bb1c577c7a5ebd6881665719bb3ba61
蒋星浩 <2812375713@qq.com>
2020-04-22 10:20:42 +0800

    提交

--- web-main/src/main/java/com/bateng/guestroom/GuestroomApplication.java
+++ web-main/src/main/java/com/bateng/guestroom/GuestroomApplication.java
@@ -17,7 +17,7 @@ public class GuestroomApplication {
 
     public static void main(String[] args) {
 
-        System.out.println();
+        System.out.println("ui");
         System.out.println();
         SpringApplication.run(GuestroomApplication.class, args);
 
