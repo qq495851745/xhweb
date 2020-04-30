c7954c97e5ebfd5429dc6a3de3f348734f6b5d86
chenchao <22190329366@qq.com>
2020-04-30 14:34:46 +0800

    更新

--- web-main/src/main/java/com/bateng/guestroom/entity/Subject.java
+++ web-main/src/main/java/com/bateng/guestroom/entity/Subject.java
@@ -1,12 +1,50 @@
 package com.bateng.guestroom.entity;
 
+import javax.persistence.*;
+import java.io.File;
+
 /**
  * 类别实体  图书的类目。科目  无限层级关系
  */
+@Entity
+@Table(name = "t_subject")
 public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sid")
+    private  Integer id;
+
+    @Column(name = "sname")
+    private String name;
 
     /**
      * 科目的描述，说明
      */
-    private String des;
+    @Column(name = "sdesc")
+    private String desc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
