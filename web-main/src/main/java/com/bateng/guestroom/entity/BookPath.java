162138271c0acece196a6a0622444b0b34b05ea7
bateng <fjl0120@163.com>
2020-04-30 09:39:20 +0800

    提交

---
+++ web-main/src/main/java/com/bateng/guestroom/entity/BookPath.java
@@ -0,0 +1,7 @@
+package com.bateng.guestroom.entity;
+
+/**
+ * 图书实体，保存存放的图书
+ */
+public class BookPath {
+}
---
+++ web-main/src/main/java/com/bateng/guestroom/entity/Subject.java
@@ -0,0 +1,12 @@
+package com.bateng.guestroom.entity;
+
+/**
+ * 类别实体  图书的类目。科目  无限层级关系
+ */
+public class Subject {
+
+    /**
+     * 科目的描述，说明
+     */
+    private String des;
+}
