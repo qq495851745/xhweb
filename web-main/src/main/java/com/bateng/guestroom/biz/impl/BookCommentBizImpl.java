8871c086dc464358f051e0a0516bc2561cb6c31e
chenchao <22190329366@qq.com>
2020-05-25 10:37:51 +0800

    更新

--- web-main/src/main/java/com/bateng/guestroom/biz/impl/BookCommentBizImpl.java
+++ web-main/src/main/java/com/bateng/guestroom/biz/impl/BookCommentBizImpl.java
@@ -4,7 +4,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.*;
 import com.bateng.guestroom.biz.BookCommentBiz;
 import com.bateng.guestroom.biz.BookUploadBiz;
+import com.bateng.guestroom.biz.SubjectBiz;
 import com.bateng.guestroom.dao.BookCommentDao;
+import com.bateng.guestroom.dao.SubjectDao;
 import com.bateng.guestroom.entity.Book;
 import com.bateng.guestroom.entity.Comment;
 import com.bateng.guestroom.entity.PageVo;
@@ -14,7 +16,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service(value = "bookCommentBiz")
 public class BookCommentBizImpl implements BookCommentBiz {
@@ -22,6 +27,10 @@ public class BookCommentBizImpl implements BookCommentBiz {
     private BookCommentDao bookCommentDao;
     @Autowired
     private BookUploadBiz bookUploadBiz;
+    @Autowired
+    private SubjectBiz subjectBiz;
+    @Autowired
+    private SubjectDao subjectDao;
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED)
@@ -101,5 +110,52 @@ public class BookCommentBizImpl implements BookCommentBiz {
         List<Comment> comments =bookCommentDao.findCommentByCon(comment.getBook().getId(),comment.getCommentContent());
         return  comments.size()==0?null:comments.get(0);
 //        return bookCommentDao.findCommentByCon(comment.getBook().getId(),comment.getCommentContent()).get(0);
+    }
+
+    @Override
+    public Subject findSubjectBySubjectId(Book book) {
+        return bookCommentDao.findSubjectBySubjectId(book.getSubject().getId());
+    }
+
+    @Override
+    public Subject findSubjectByPid(Subject subject) {
+          return subject.getSubject()==null?subject:findSubjectByPid(subject.getSubject());
+//        if(subject.getSubject()==null){
+//            return subject;
+//        }
+//        else if(subject.getSubject().getId()!=0){
+//            findSubjectByPid(subject.getSubject());
+//        }
+//
+//        return  null;
+
+
+    }
+
+    @Override
+    public String findSubjectByBookAjax() {
+        List<Book> books = bookUploadBiz.findAll();
+        List<Subject> subjects = new ArrayList<Subject>();
+//        List<Map<Object,Subject>> maps = new ArrayList<Map<Object,Subject>>();
+        List<Map<String,Object>> maps = new ArrayList<Map<String,Object>>();
+//        Map<Object,Subject> map = new HashMap<Object,Subject>();
+        Map<String,Object> map = new HashMap<String,Object>();
+        for(Book book : books){
+            Subject subject = findSubjectBySubjectId(book);
+            map=new HashMap<String, Object>();
+            subjects.add(subject);
+            Subject subject1 = findSubjectByPid(subject);
+            map.put("id",book.getId());
+            map.put("name",book.getName());
+            map.put("pId",subject1.getId());
+            maps.add(map);
+        }
+        return JSONObject.toJSONString(maps,new SerializeFilter[]{},SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    @Override
+    public String getBookCommentNum() {
+        List<Object> list = bookCommentDao.getBookCommentNum();
+        return JSONObject.toJSONString(list);
     }
 }
